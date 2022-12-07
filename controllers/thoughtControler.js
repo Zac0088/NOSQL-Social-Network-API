ab4d1e9ba81deb04a2007d53eaf13128034e4675
Zac Smart <zac_chaz@outlook.com>
2022-12-07 14:33:39 +1100

    edited starter boilerplate from mini project, need to implement create reactions and delete

--- controllers/thoughtControler.js
+++ controllers/thoughtControler.js
@@ -1,13 +1,13 @@
 const { thought, user } = require('../models');
 
 module.exports = {
-  // Get all courses
+  // Get all thoughts
   getThought(req, res) {
     thought.find()
       .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a course
+  // Get a thought
   getSingleThought(req, res) {
     thought.findOne({ _id: req.params.thoughtId })
       .select('-__v')
@@ -18,7 +18,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Create a course
+  // Create a thought
   createThought(req, res) {
     thought.create(req.body)
       .then((thought) => {
@@ -37,28 +37,28 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
-  // Delete a course
-  deleteCourse(req, res) {
-    Course.findOneAndDelete({ _id: req.params.courseId })
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : Student.deleteMany({ _id: { $in: course.students } })
+  // Delete a thought
+  deleteThought(req, res) {
+    Course.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : user.deleteMany({ _id: { $in: thought.user } })
       )
-      .then(() => res.json({ message: 'Course and students deleted!' }))
+      .then(() => res.json({ message: 'Thought deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
-  // Update a course
-  updateCourse(req, res) {
-    Course.findOneAndUpdate(
-      { _id: req.params.courseId },
+  // Update a thought
+  updateThought(req, res) {
+    thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
       { $set: req.body },
-      { runValidators: true, new: true }
+      { new: true }
     )
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with this id!' })
-          : res.json(course)
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this id!' })
+          : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
   },
